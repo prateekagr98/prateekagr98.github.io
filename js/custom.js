3b78d5ff75f552d25829c4227e1d5203643a83f7
Prateek Agarwal <prateekagr98@gmail.com>
2014-06-12 19:13:21 +0530

    Changed the website design and added contents

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,32 @@
+var isClicked = "false";
+
 $(document).ready(function(){
-	set_logo();
+
+
+	$('.find_me').click(function(){
+
+		if(isClicked == "false"){
+			$('.find_me').html(":-)");
+			showSocialLinks();
+			isClicked = "true";
+		} else{
+			$('.find_me').html("Find Me");
+			showSocialLinks();
+			isClicked = "false";
+		}
+	});
+
 });
 
-$(window).resize(function(){
-	set_logo();
-});
+function showSocialLinks(){
+	TweenMax.from('.gmail', 0.5, {css:{top:"40%", left: "40%", visibility: "visible"}, ease:Power2.easeOut});
+	TweenMax.from('.twitter', 0.5, {css:{top:"40%", right: "40%", visibility: "visible"}, ease:Power2.easeOut});
+	TweenMax.from('.facebook', 0.5, {css:{bottom:"40%", right: "40%", visibility: "visible"}, ease:Power2.easeOut});
+	TweenMax.from('.linkedin', 0.5, {css:{bottom:"40%", left: "40%", visibility: "visible"}, ease:Power2.easeOut});
 
-function set_logo(){
-	if($(window).width() <= 728){
-		$('.intro').hide();
-		$('.logo').show();
-	} else {
-		$('.intro').show();
-		$('.logo').hide();
-	}
+	TweenMax.to('.gmail', 0.5, {css:{visibility: "visible"}});
+	TweenMax.to('.twitter', 0.5, {css:{visibility: "visible"}});
+	TweenMax.to('.facebook', 0.5, {css:{visibility: "visible"}});
+	TweenMax.to('.linkedin', 0.5, {css:{visibility: "visible"}});
 }
+
