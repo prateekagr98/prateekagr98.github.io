a160c47b6575628856debd787b583647887929c7
Prateek Agarwal <prateekagr98@gmail.com>
2014-09-04 23:50:21 +0530

    UI changes for the website

--- js/custom.js
+++ js/custom.js
@@ -2,31 +2,6 @@ var isClicked = "false";
 
 $(document).ready(function(){
 
-
-	$('.find_me').click(function(){
-
-		if(isClicked == "false"){
-			$('.find_me').html(":-)");
-			showSocialLinks();
-			isClicked = "true";
-		} else{
-			$('.find_me').html("Find Me");
-			showSocialLinks();
-			isClicked = "false";
-		}
-	});
-
+	$('.homepage').height($(window).innerHeight());
 });
-
-function showSocialLinks(){
-	TweenMax.from('.gmail', 0.5, {css:{top:"40%", left: "40%", visibility: "visible"}, ease:Power2.easeOut});
-	TweenMax.from('.twitter', 0.5, {css:{top:"40%", right: "40%", visibility: "visible"}, ease:Power2.easeOut});
-	TweenMax.from('.facebook', 0.5, {css:{bottom:"40%", right: "40%", visibility: "visible"}, ease:Power2.easeOut});
-	TweenMax.from('.linkedin', 0.5, {css:{bottom:"40%", left: "40%", visibility: "visible"}, ease:Power2.easeOut});
-
-	TweenMax.to('.gmail', 0.5, {css:{visibility: "visible"}});
-	TweenMax.to('.twitter', 0.5, {css:{visibility: "visible"}});
-	TweenMax.to('.facebook', 0.5, {css:{visibility: "visible"}});
-	TweenMax.to('.linkedin', 0.5, {css:{visibility: "visible"}});
-}
 
